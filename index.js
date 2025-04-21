66eb8fd7e64ba63e45b8af50d1584446f5921655
rohitsingh <rohitsin28@gmail.com>
2025-04-21 23:34:55 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from "express";
 
 const app = express();
 
-
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000`)
+app.use(express.json());
+app.use("/",(req,res)=>{
+    return res.send("Server is working fine")
 })
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
